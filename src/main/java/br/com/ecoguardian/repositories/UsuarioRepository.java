564384b905b2a6ef75c393b4e51bb366c60249f5
Thiago Chirana <engsw.thiago@gmail.com>
2023-11-13 00:17:31 -0300

    FEATURE | Adicao do Usuario Repository ao projeto

---
+++ src/main/java/br/com/ecoguardian/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package br.com.ecoguardian.repositories;
+
+import br.com.ecoguardian.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
