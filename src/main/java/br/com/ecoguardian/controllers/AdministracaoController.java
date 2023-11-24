4db55d73b77e126f4fb4e84cfd9250c83acbe1d9
Thiago Chirana <engsw.thiago@gmail.com>
2023-11-24 00:10:20 -0300

    FEATURE | Upload de arquivos formato imagem

--- src/main/java/br/com/ecoguardian/controllers/AdministracaoController.java
+++ src/main/java/br/com/ecoguardian/controllers/AdministracaoController.java
@@ -2,10 +2,13 @@ package br.com.ecoguardian.controllers;
 
 import br.com.ecoguardian.models.records.NovoUsuarioJSON;
 import br.com.ecoguardian.models.enums.TipoPerfil;
+import br.com.ecoguardian.services.AdministracaoService;
 import br.com.ecoguardian.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -19,6 +22,9 @@ public class AdministracaoController {
 
     @Autowired
     private UsuarioService usuarios;
+
+    @Autowired
+    private AdministracaoService admService;
 
     @GetMapping
     public ModelAndView getAdminPage(){
@@ -37,6 +43,27 @@ public class AdministracaoController {
     @GetMapping("localizacao")
     public ModelAndView localizacaoTeste(){
         return new ModelAndView("localizacao/localizacao");
+    }
+
+
+    //Exemplo imagens
+    @PostMapping("/upload")
+    public String salvarArquivo(@RequestParam("arquivos") List<MultipartFile> arquivos){
+        admService.salvarArquivos(arquivos);
+        return "redirect:/admin/imagens";
+    }
+
+    @GetMapping("/imagens")
+    public ModelAndView abrirTelaImagens(){
+        ModelAndView model = new ModelAndView("imagens");
+        model.addObject("imagens", admService.listarTodos());
+        return model;
+    }
+
+    @GetMapping("/verImagem/{id}")
+    @ResponseBody
+    public ResponseEntity<byte[]> exibirImagem(@PathVariable Long id){
+        return admService.obterImagem(id);
     }
 
 }
--- src/main/java/br/com/ecoguardian/services/AdministracaoService.java
+++ src/main/java/br/com/ecoguardian/services/AdministracaoService.java
@@ -1,7 +1,19 @@
 package br.com.ecoguardian.services;
 
+import br.com.ecoguardian.models.Arquivo;
+import br.com.ecoguardian.repositories.ArquivoRepository;
+import br.com.ecoguardian.utils.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AdministracaoService {
@@ -13,5 +25,41 @@ public class AdministracaoService {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Autowired
+    private ArquivoRepository arquivosRepository;
 
+    Log LOG = new Log(AdministracaoService.class);
+
+    public void salvarArquivos(List<MultipartFile> arquivos){
+        try {
+            for (MultipartFile arq : arquivos) {
+                if (!arq.isEmpty()) {
+                    String nomeArquivoOriginal = arq.getOriginalFilename();
+                    Path path = Paths.get(nomeArquivoOriginal);
+                    String nomeSemExtensao = path.getFileName().toString().replaceFirst("[.][^.]+$", "");
+                    String formato = path.getFileName().toString().substring(nomeSemExtensao.length() + 1);
+                    Arquivo arquivo = new Arquivo(arq.getBytes(), nomeSemExtensao, formato);
+                    arquivosRepository.save(arquivo);
+                }
+            }
+        } catch(Exception e) {
+            LOG.erro("Houve um erro ao carregar arquivo", e);
+        }
+    }
+
+    public List<Arquivo> listarTodos(){
+        return arquivosRepository.findAll();
+    }
+
+    public ResponseEntity<byte[]> obterImagem(Long id){
+        Optional<Arquivo> arq = arquivosRepository.findById(id);
+        if (arq.isPresent()){
+            Arquivo arquivo = arq.get();
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + arquivo.getNome()+"."+arquivo.getFormato() + "\"")
+                    .body(arquivo.getArquivo());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
