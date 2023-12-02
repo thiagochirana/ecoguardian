990e014c11e25bd46b07e4e2ee979024a6b88274
Thiago Chirana Macedo <96755607+thiagochirana@users.noreply.github.com>
2023-12-02 17:46:27 -0300

    Ajuste para exibição de Denúncias feitas para Usuario e Analista(#13)

--- src/main/java/br/com/ecoguardian/models/Arquivo.java
+++ src/main/java/br/com/ecoguardian/models/Arquivo.java
@@ -4,6 +4,7 @@ import br.com.ecoguardian.utils.Datas;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 
 import java.util.Date;
 
--- src/main/java/br/com/ecoguardian/services/DenunciaService.java
+++ src/main/java/br/com/ecoguardian/services/DenunciaService.java
@@ -7,14 +7,11 @@ import br.com.ecoguardian.models.records.DenunciaJSON;
 import br.com.ecoguardian.repositories.DenunciaRepository;
 import br.com.ecoguardian.utils.Datas;
 import br.com.ecoguardian.utils.Log;
-import br.com.ecoguardian.utils.ParametrosInicializacao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class DenunciaService {
@@ -59,7 +56,7 @@ public class DenunciaService {
         return denuncias.save(arquivos.salvarArquivosDaDenuncia(denSalva, listaArquivos));
     }
 
-    public Denuncia abrirDenunciaAnonima(DenunciaJSON json, List<MultipartFile> listaArquivos){
+    public void abrirDenunciaAnonima(DenunciaJSON json, List<MultipartFile> listaArquivos){
         Municipio municipio = municipioService.obterMunicipio(json.idIBGE());
         Denuncia denNova = new Denuncia(json, usuarioService.obterNovoAnonimo(), municipio);
         Localizacao local = localizacaoService.salvar(denNova.getLocalizacao());
@@ -70,7 +67,7 @@ public class DenunciaService {
         RegistroDenuncia registro = registroDenunciaService.abrir(denSalva);
         gerarNumeroProtocolo(denSalva);
         denSalva.adicionarRegistro(registro);
-        return denuncias.save(arquivos.salvarArquivosDaDenuncia(denSalva, listaArquivos));
+        denuncias.save(arquivos.salvarArquivosDaDenuncia(denSalva, listaArquivos));
     }
 
     public Denuncia abrir(DenunciaJSON json){
@@ -128,6 +125,29 @@ public class DenunciaService {
             LOG.warn("Usuario Logado está nulo", nulo);
             return new ArrayList<>();
         }
+    }
+
+    public List<Denuncia> todasAbertasDoUsuarioLogado(){
+        return denuncias.listarTodasEmAbertoDoUsuario(sessaoServiceWrapper.getUsuarioLogado()).orElseGet(ArrayList::new);
+    }
+
+
+    public List<Denuncia> todasEmAnaliseDoAnalista(){
+        Set<Denuncia> den = new HashSet<>();
+        for (RegistroDenuncia rd : registroDenunciaService.todasAtualizadasPeloAnalista()){
+            if (rd.getDenuncia().getStatusDenuncia() == StatusDenuncia.EM_ANALISE){
+                den.add(rd.getDenuncia());
+            }
+        }
+        return den.stream().toList();
+    }
+
+    public List<Denuncia> todasAbertasOuEmAnalise(){
+        List<Denuncia> lista = new ArrayList<>();
+        lista.addAll(denuncias.todasAbertas().orElseGet(ArrayList::new));
+        List<Denuncia> lisAux = todasEmAnaliseDoAnalista();
+        lista.addAll(lisAux);
+        return lista;
     }
 
     public List<Denuncia> listarTodas(){
